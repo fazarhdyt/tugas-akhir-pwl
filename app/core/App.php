99e9815c1c3c33b574d47beae8e814aadf970091
fazarhdyt <m.fajar184@gmail.com>
2024-07-12 23:31:05 +0700

    fix error home page

--- app/core/App.php
+++ app/core/App.php
@@ -7,10 +7,12 @@ class App {
     public function __construct() {
         $url = $this->parseURL();
         
-        //controller
-        if(file_exists('../app/controllers/' .$url[0] .'.php')) {
-            $this->controller = $url[0];
-            unset($url[0]);
+        if(is_array($url) && !empty($url)) {
+            //controller
+            if(file_exists('../app/controllers/' .$url[0] .'.php')) {
+                $this->controller = $url[0];
+                unset($url[0]);
+            }
         }
         require_once '../app/controllers/' .$this->controller .'.php';
         $this->controller = new $this->controller;
